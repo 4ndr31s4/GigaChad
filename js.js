464d0ebb01a70f3ae6b5ee8019547fcf5acfc55c
Andrei dos Santos Pinto <107800966+4ndr31s4@users.noreply.github.com>
2023-01-12 11:19:49 -0300

    Add files via upload

---
+++ js.js
@@ -0,0 +1,9 @@
+/*var corpo = window.document.body
+var link = window.document.body.getElementsByTagName('span')
+
+function trocarTema(){
+     corpo.style.background = 'white'
+     corpo.style.color = 'black'
+     link.style.color = 'black'
+}
+*/
